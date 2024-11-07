4eb6d424a5ac01b86f9d55137bb0080e6a34264f
Nas103 <tumelorhulani33@gmail.com>
2024-11-07 11:37:59 +0000

    pi

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,21 +38,14 @@ def filter_datum(
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-    """
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
 
-        def format(self, record: logging.LogRecord) -> str:
-            original_message = super().format(record)
-        return filter_datum(
-            self.fields,
-            self.REDACTION,
-            original_message,
-            self.SEPARATOR
-        )
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
