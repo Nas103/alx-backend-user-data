c63d106bd0065bf283efb6e8d7c597e1881611c8
Nas103 <tumelorhulani33@gmail.com>
2024-11-07 11:35:08 +0000

    pi

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Module for filtering log messages.
 """
 
+import logging
 import re
 from typing import List
 
@@ -33,3 +34,25 @@ def filter_datum(
         lambda m: f'{m.group(0).split("=")[0]}={redaction}',
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+        def format(self, record: logging.LogRecord) -> str:
+            original_message = super().format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR
+        )
