9220a034f5b5c2ee157d723519a6c3e1d9e68d98
Nas103 <tumelorhulani33@gmail.com>
2024-11-21 20:27:16 +0000

    sql

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+
+    Args:
+        password (str): The password string to be hashed.
+
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    salt = bcrypt.gensalt()  # Generate a salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
